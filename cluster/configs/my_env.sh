12daeb8c1dc6a86af0133b92be16fe9e2409e568
unknown <yyzdexinlang@sina.com>
2023-04-02 21:44:35 +0800

    configs

---
+++ cluster/configs/my_env.sh
@@ -0,0 +1,9 @@
+export KAFKA_HOME=/opt/module/kafka
+export PATH=$PATH:$KAFKA_HOME/bin
+
+export JAVA_HOME=/opt/module/jdk1.8.0_212
+export PATH=$PATH:$JAVA_HOME/bin
+
+export HADOOP_HOME=/opt/module/hadoop-3.1.3
+export PATH=$PATH:$HADOOP_HOME/bin
+export PATH=$PATH:$HADOOP_HOME/sbin
