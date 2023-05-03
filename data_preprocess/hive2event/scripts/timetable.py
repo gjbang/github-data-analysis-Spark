b93655ae6cc73a02a653aef5ed92e7164593d1e7
gjbang <793881807@qq.com>
2023-05-04 01:52:31 +0800

    [UPDATE] add preprocess

--- data_preprocess/hive2event/scripts/timetable.py
+++ data_preprocess/hive2event/scripts/timetable.py
@@ -1,5 +1,6 @@
 import time
 import datetime
+
 from pyspark.sql import functions as F
 from pyspark.sql.types import StringType
 from pyspark.sql import HiveContext,SparkSession
@@ -14,49 +15,33 @@ spark = SparkSession.builder.master(_SPARK_HOST)\
 
 hive_context = HiveContext(spark)
 
-def get_time(hours):
-    currentTime = (datetime.datetime.now() - datetime.timedelta(hours=hours)).strftime("%Y-%m-%d-%H-%A")
-    print(currentTime)
-    times = currentTime.split("-")
-    year = times[0]
-    month = times[1]
-    day = times[2]
-    hour = times[3]
-    week = times[4]
-    if week == "Sunday" or week == "Saturday":
-        rest = "rest"
-    else:
-        rest = "not-rest"
-    if int(hour) > 7 and int(hour) < 18:
-        daytime = "day_time"
-    else:
-        daytime = "night_time"
-    return year, month, day, hour, week, rest, daytime
+read_df = hive_context.sql("select created_at from default.ods_activitytable limit 1")
 
-l1 = "asia"
-y1, m1, d1, h1, w1, r1, n1 = get_time(0)
-l2 = "europe"
-y2, m2, d2, h2, w2, r2, n2 = get_time(-4)
-l3 = "america"
-y3, m3, d3, h3, w3, r3, n3 = get_time(-12)
+def time_hour(time):
+    times = time.split('T')
+    hour  = times[1].split(':')[0]
+    return times[0] + "-" + hour
+
+def time_day(time):
+    times = time.split('T')
+    return times[0]
+
+time_transformUDF = F.udf(time_hour, StringType())
+time_dayUDF = F.udf(time_day, StringType())
 
 
-times = [(l1, y1, m1, d1, h1, w1, r1, n1),
-          (l2, y2, m2, d2, h2, w2, r2, n2),
-          (l3, y3, m3, d3, h3, w3, r3, n3)]
-
-print(times)
-schema = ['location', 'year', 'month', 'day', 'hour', 'week', 'rest', 'daytime']
-inserts = spark.createDataFrame(times, schema)
-
+inserts = read_df.withColumn("timestamp", F.lit(time.time()))
+inserts = inserts.withColumn("time", time_transformUDF(inserts.created_at))
+inserts = inserts.withColumn("time_day", time_dayUDF(inserts.created_at))
+inserts = inserts.drop('created_at')
 
 inserts.registerTempTable('tmptable')
-s = spark.sql("show tables in default like 'timeTable'")
+s = spark.sql("show tables in default like 'timesTable'")
 flag = len(s.collect())
 if flag:
     print("exist")
-    spark.sql("insert into default.timeTable select * from tmptable")
+    spark.sql("insert into default.timesTable select * from tmptable")
 else:
     print("not exist")
-    spark.sql("create table IF NOT EXISTS default.timeTable select * from tmptable")
+    spark.sql("create table IF NOT EXISTS default.timesTable select * from tmptable")
 #
